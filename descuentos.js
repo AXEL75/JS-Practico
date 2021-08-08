5ceffeea85cd5c60824872c03627c71aee78a2d4
Axel Valero <axel16valerosanchez@gmail.com>
2021-08-08 15:44:14 -0500

    Cupones de descuento

--- descuentos.js
+++ descuentos.js
@@ -1,9 +1,23 @@
-const precioOriginal = 120;
-const descuento = 18;
+var cupos = function () {
+    const cupons = [10, 25, 50,75];
+    const cupon = document.getElementById("cupons");
+    const valueCupon = parseInt(cupon.value);
+    if (valueCupon === cupons[0]) {
+        return 0.1;
+    } else if (valueCupon === cupons[1]) {
+        return 0.25;
+    } else if (valueCupon === cupons[2]) {
+        return 0.50;
+    } else if (valueCupon === cupons[3]) {
+        return 0.75;
+    }else {
+        return 0
+    }
+}
 
-function calcularPrecioConDescuento(precio,descuento){
-    const porcentajePrecioConDescuento = 100 - descuento;
-    const precioConDescuento = (precio*porcentajePrecioConDescuento) /100;
+function calcularPrecioConDescuento(precio){
+    const porcentajePrecioConDescuento = 1 - cupos();
+    const precioConDescuento = (precio*porcentajePrecioConDescuento);
 
     return precioConDescuento;
 }
@@ -11,11 +25,7 @@ function calcularPrecioConDescuento(precio,descuento){
 function onClickButtonPriceDiscount(){
     const inputPrice = document.getElementById("InputPrice");
     const priceValue = parseInt(inputPrice.value);
-
-    const inputDiscount = document.getElementById("InputDiscount");
-    const discountValue = parseInt(inputDiscount.value);
-
-    const precioConDescuento = calcularPrecioConDescuento(priceValue,discountValue);
+    const precioConDescuento = calcularPrecioConDescuento(priceValue);
 
     const result =document.getElementById("ResultPrice");
     result.innerText = "El precio con descuento es: $" + precioConDescuento;
